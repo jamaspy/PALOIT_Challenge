e6d1139d738e13ec482230641cc908b4a3ac4c0c
jamaspy <aspin@me.com>
2019-07-12 15:25:58 +1000

    added comments

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,20 +1,22 @@
 class Person < ApplicationRecord
  def asciify
-    string = firstname + lastname 
+    #merge first and last name AND add a space between as per example in spec 
+    string = firstname + " " + lastname
+    #take each character and convert to ascii value in an array then fiond the sum of the array 
     ascii = string.chars.map(&:ord).inject(:+)
     ascii 
  end
 
  def binarify
-    #merge first and last name 
-    string = firstname + lastname
-    #take each character and convert to ascii value in an array, find the sum of the array and convert to a base 2 string 
+    string = firstname + " " + lastname
+    #take each character and convert to ascii value in an array
+    #find the sum of the array and convert to a base 2 string 
     binary = string.chars.map(&:ord).inject(:+).to_s(2) 
     binary
  end
 
  def countZeros
-    string = firstname + lastname
+    string = firstname + " " + lastname
     binary = string.chars.map(&:ord).inject(:+).to_s(2) 
     max = 0
     count = 0
