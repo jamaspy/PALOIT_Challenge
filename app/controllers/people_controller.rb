eb05faaa19bf54c2f43a34bad1de8953d7272537
jamaspy <aspin@me.com>
2019-07-13 09:09:29 +1000

    Added rack-CORS

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,10 @@
 class PeopleController < ApplicationController
   def index
     @people = Person.all
+    respond_to do |format| 
+      format.html # index.html.erb 
+      format.json { render :json => @people } 
+    end
   end
 
   def show
